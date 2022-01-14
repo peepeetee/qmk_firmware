e51a2b05c095760d5f293960c58e746bac182c09
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-01-14 13:33:58 +1100

    add a note about disabled underglow for `*_nug` keymaps

--- keyboards/id67/keymaps/default_nug/config.h
+++ keyboards/id67/keymaps/default_nug/config.h
@@ -14,6 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * NB!  THIS VARIANT DISABLES THE UNDERGLOW LEDs
+ *      FOR BOARDS WITH A SOLID (NON-ACRILIC) BOTTOM
+ */
+
 #pragma once
 
 #if defined(RGB_MATRIX_ENABLE)
