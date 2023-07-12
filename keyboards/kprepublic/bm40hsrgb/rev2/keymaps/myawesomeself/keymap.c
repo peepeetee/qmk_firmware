efb2c4417aa499b033e3fee89c81520199727f39
Zachary Gorman <zatcat6@gmail.com>
2023-07-12 13:51:23 -0700

    kprepublic/bm40hsrgb now has a rev1 and rev2 keyboard, rev2 now correctly flashes and the default layout is cleaned up.

---
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/myawesomeself/keymap.c
@@ -0,0 +1,204 @@
+/* Copyright 2022 bdtc123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum my_keycodes {
+    RMT = SAFE_RANGE,
+    RMS,
+    RMIH,
+    RMDH,
+    RMIS,
+    RMDS,
+    RMIV,
+    RMDV
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RMT:
+      if (record->event.pressed)
+      {rgb_matrix_toggle();
+      }
+      return false;
+    case RMS:
+      if (record->event.pressed)
+      {rgb_matrix_step();
+      }
+      return false;
+      case RMIH:
+      if (record->event.pressed)
+      {rgb_matrix_increase_hue();
+      }
+      return false;
+      case RMDH:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_hue();
+      }
+      return false;
+      case RMIS:
+      if (record->event.pressed)
+      {rgb_matrix_increase_sat();
+      }
+      return false;
+      case RMDS:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_sat();
+      }
+      return false;
+      case RMIV:
+      if (record->event.pressed)
+      {rgb_matrix_increase_val();
+      }
+      return false;
+      case RMDV:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_val();
+      }
+      return true;
+      default:
+      return true;
+  }
+}
+
+enum layers {
+  _COLEMAK,
+  _DVORAK,
+  _QWERTY,
+  _NUM,
+  _VIM,
+  _ADJUST
+};
+
+#define HL_NUM MO(_NUM)
+// #define HL_SYM MO(_SYM)
+#define VIM_SPC LT(_VIM, KC_SPC)
+#define HM_LYR TO(_COLEMAK)
+#define CLMK_LYR TO(_COLEMAK)
+#define QWRTY_LYR TO(_QWERTY)
+#define DVRK_LYR TO(_DVORAK)
+#define RGB_LYR TO(_ADJUST)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  
+/* Colemak 
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | Med  | NUM  |  vim-Space  | SYM  | Bksp | Bckl | HmLr | Qwrt |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT_planck_mit(
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_SLSH,
+    KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY, HL_NUM,  VIM_SPC, KC_BSPC, HM_LYR,  RMT,     RGB_LYR, QWRTY_LYR
+),
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | Med  | NUM  |    Space    | SYM  | Bksp | Bckl | HmLr | Dvrk |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_planck_mit(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY, HL_NUM,  KC_SPC,  KC_BSPC, HM_LYR,  RMT,     RGB_LYR, DVRK_LYR
+),
+
+/* Dvorak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  |  /   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  | Med  | NUM  |  vim-Space  | SYM  | Bksp | Bckl | HmLr | Qwrt |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_DVORAK] = LAYOUT_planck_mit(
+    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+    KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY, HL_NUM,  VIM_SPC, KC_BSPC, HM_LYR,  RMT,     RGB_LYR, CLMK_LYR
+),
+
+/* Vim 
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |Paste | Copy |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | End  |      |      | Undo | Home |      | Left | Down |  Up  |Right |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_VIM] = LAYOUT_planck_mit(
+    _______, _______, _______, _______, C(KC_V), C(KC_C), _______, _______, _______,  _______,  _______, _______,
+    _______, KC_END,  _______, _______, C(KC_Z), KC_HOME, _______, KC_LEFT, KC_DOWN,  KC_UP,    KC_RGHT, _______,
+    KC_LSFT, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______
+),
+
+/* Num
+ * ,-----------------------------------------------------------------------------------.
+ * |  `   |      |      |      |      |      |   A  |  7   |  8   |  9   |  /   |  *   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  (   |  )   |  [   |  ]   |  {   |  }   |   B  |  4   |  5   |  6   |  +   |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |  \   |  |   |   C  |  1   |  2   |  3   |  .   |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |          D  |  0   |  E   |  F   |  =   |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NUM] = LAYOUT_planck_mit(
+    KC_GRV,  _______, _______, _______, _______, _______, KC_A,  KC_7,  KC_8, KC_9, KC_SLSH, KC_PAST,
+    KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_B,  KC_4,  KC_5, KC_6, KC_PLUS, KC_MINS,
+    _______, _______, _______, _______, KC_BSLS, KC_PIPE, KC_C,  KC_1,  KC_2, KC_3, KC_DOT, KC_ENT,
+    KC_CAPS, _______, _______, _______, _______,          KC_D,  KC_0,  KC_E, KC_F, KC_EQL, HM_LYR
+),
+
+/* Adjust (Lower + Raise)
+ *                      v------------------------RGB CONTROL--------------------v
+ * ,-----------------------------------------------------------------------------------.
+ * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = LAYOUT_planck_mit(
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, _______, _______, RMT,     RMS,     RMIH,    RMDH,    RMIS,    RMDS,    RMIV,    RMDV,    _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, HM_LYR
+)
+
+};
